f49b1d4d61b92f06981d888faad0c9df7bd7f048
Martin White <martin.white@laterooms.com>
2016-10-04 17:48:25 +0100

    Revert "Worker queues"
    This reverts commit 2e441c76934dacb04d1aeccea7af5808d6467713.

--- Receiver/RabbitMQReceiver/RabbitMQReceiver/Program.cs
+++ Receiver/RabbitMQReceiver/RabbitMQReceiver/Program.cs
@@ -2,7 +2,6 @@
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
-using System.Threading;
 
 namespace RabbitMQReceiver
 {
@@ -14,15 +13,11 @@ namespace RabbitMQReceiver
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "task_queue",
-                                     durable: true,
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
                                      exclusive: false,
                                      autoDelete: false,
                                      arguments: null);
-
-                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
-
-                Console.WriteLine(" [*] Waiting for messages.");
 
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
@@ -30,16 +25,10 @@ namespace RabbitMQReceiver
                     var body = ea.Body;
                     var message = Encoding.UTF8.GetString(body);
                     Console.WriteLine(" [x] Received {0}", message);
-
-                    int dots = message.Split('.').Length - 1;
-                    Thread.Sleep(dots * 1000);
-
-                    Console.WriteLine(" [x] Done");
-
-                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    System.Threading.Thread.Sleep(new System.Random().Next(1000));
                 };
-                channel.BasicConsume(queue: "task_queue",
-                                     noAck: false,
+                channel.BasicConsume(queue: "hello",
+                                     noAck: true,
                                      consumer: consumer);
 
                 Console.WriteLine(" Press [enter] to exit.");
--- Sender/RabbitSender/RabbitSender/Program.cs
+++ Sender/RabbitSender/RabbitSender/Program.cs
@@ -6,38 +6,36 @@ namespace RabbitSender
 {
     class Send
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "task_queue",
-                                     durable: true,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
 
-                var message = GetMessage(args);
-                var body = Encoding.UTF8.GetBytes(message);
 
-                var properties = channel.CreateBasicProperties();
-                properties.Persistent = true;
+                    for (int i = 0; i < 1000000000; i++)
+                    {
+                        string message = "Hello World! " + i;
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: "",
+                            routingKey: "hello",
+                            basicProperties: null,
+                            body: body);
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }
 
-                channel.BasicPublish(exchange: "",
-                                     routingKey: "task_queue",
-                                     basicProperties: properties,
-                                     body: body);
-                Console.WriteLine(" [x] Sent {0}", message);
+                }
             }
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
-        }
-
-        private static string GetMessage(string[] args)
-        {
-            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
         }
     }
 }
