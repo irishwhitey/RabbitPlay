1a7507c56939b89e674b911121e4d2847a98614b
Martin White <martin.white@laterooms.com>
2016-10-06 16:35:08 +0100

    Switched to durable queues with persisted messages

--- Sender/RabbitSender/RabbitSender/Program.cs
+++ Sender/RabbitSender/RabbitSender/Program.cs
@@ -6,36 +6,35 @@ namespace RabbitSender
 {
     class Send
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
                 {
-                    channel.QueueDeclare(queue: "hello",
-                        durable: false,
-                        exclusive: false,
-                        autoDelete: false,
-                        arguments: null);
+                    channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
 
-                    for (int i = 0; i < 1000000000; i++)
-                    {
-                        string message = "Hello World! " + i;
-                        var body = Encoding.UTF8.GetBytes(message);
-                        channel.BasicPublish(exchange: "",
-                            routingKey: "hello",
-                            basicProperties: null,
-                            body: body);
-                        Console.WriteLine(" [x] Sent {0}", message);
-                    }
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+
 
                 }
             }
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
+        }
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
         }
     }
 }
